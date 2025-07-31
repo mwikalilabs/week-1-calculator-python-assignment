e7984bd48b6717153a873926d9710fb59f80555a
Mwikali <mwikalisharon749@gmail.com>
2025-07-31 14:19:52 +0300

    Add files via upload

---
+++ week-1 assignment.py
@@ -0,0 +1,22 @@
+#user to input two numbers
+number1 = float(input("enter your first number:"))
+number2 = float(input("enter your first number:"))
+
+#add two numbers
+addition = number1 + number2
+
+#substract two numbers
+subtraction = number1 - number2
+
+#multiply two numbers
+multiplication = number1 * number2
+
+#divide two numbers
+division = number1 / number2
+
+#print out results
+print("Welcome to my Calculator App")
+print(f"addition:{addition}")
+print(f"subtraction:{subtraction}")
+print(f"multiplication:{multiplication}")
+print(f"division:{division}")
